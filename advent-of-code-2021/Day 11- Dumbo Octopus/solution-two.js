15e8f2a81ee47424871c5e2d0acf38620c23c5a1
Adigun Palmer <apalmer@electroncorp.onmicrosoft.com>
2021-12-20 14:16:59 -0500

    Day 11

---
+++ advent-of-code-2021/Day 11- Dumbo Octopus/solution-two.js
@@ -0,0 +1,135 @@
+const fs = require('fs')
+
+function raiseEnergy(universe) {
+    for (let y = 0; y < universe.data.length; y++) {
+        for (let x = 0; x < universe.data[y].length; x++) {
+            universe.data[y][x]++;
+        }
+    }
+
+    return universe;
+}
+
+function getNeighbors(universe, y, x) {
+
+    let possibles = [
+        { x: x - 1, y: y - 1 },
+        { x: x, y: y - 1 },
+        { x: x + 1, y: y - 1 },
+
+        { x: x - 1, y: y },
+        { x: x + 1, y: y },
+
+        { x: x - 1, y: y + 1 },
+        { x: x, y: y + 1 },
+        { x: x + 1, y: y + 1 }
+    ];
+
+    let actuals = possibles.filter(p => p.x >= 0 && p.x < universe.data[0].length && p.y >= 0 && p.y < universe.data.length);
+
+    return actuals;
+}
+
+function flashOctopus(universe, y, x) {
+
+    if (universe.data[y][x] > 9) {
+        let alreadyFlashed = universe.state.flashed.find(f => f.x === x && f.y === y);
+        if (!alreadyFlashed) {
+            universe.state.flashCount++;
+            universe.state.flashed.push({ x: x, y: y });
+
+            let neighbors = getNeighbors(universe, y, x);
+
+            neighbors.forEach(neighbor => {
+                universe.data[neighbor.y][neighbor.x]++;
+            });
+
+            neighbors.forEach(neighbor => {
+                universe = flashOctopus(universe, neighbor.y, neighbor.x);
+            });
+        }
+    }
+
+    return universe;
+}
+
+function flashOctopi(universe) {
+
+    for (let y = 0; y < universe.data.length; y++) {
+        for (let x = 0; x < universe.data[y].length; x++) {
+            universe = flashOctopus(universe, y, x);
+        }
+    }
+
+    return universe;
+}
+
+function resetFlashed(universe) {
+
+    universe.state.flashed.forEach(element => {
+        universe.data[element.y][element.x] = 0;
+    });
+
+    let numFlashed = universe.state.flashed.length;
+    universe.state.flashed = [];
+
+    universe.state.current++;
+
+    if (numFlashed == universe.data.length * universe.data[0].length) {
+        if (!universe.state.allFlashed) {
+            universe.state.allFlashed = universe.state.current;
+        }
+    }
+
+
+    return universe;
+}
+
+function iterate(universe, iterations) {
+    if (iterations == 0) {
+        return universe;
+    }
+    else {
+        let raised = raiseEnergy(universe);
+        let flashed = flashOctopi(raised);
+        let resetted = resetFlashed(flashed);
+
+        return iterate(resetted, iterations - 1);
+    }
+}
+
+function renderVisual(grid) {
+
+    let vis = '';
+
+    for (let y = 0; y < grid.length; y++) {
+        for (let x = 0; x < grid[y].length; x++) {
+            vis += grid[y][x];
+            //vis += "|"
+        }
+        vis += "\r\n";
+    }
+
+    return vis;
+}
+
+const iterations = 300;
+
+const rawData = fs.readFileSync('data.txt', 'utf8')
+    .toString()
+    .split("\r\n");
+
+const data = rawData.map(x => x.split('').map(Number));
+const state = { flashed: [], flashCount: 0, current: 0 };
+const universe = {
+    data: data,
+    state: state
+};
+
+let results = iterate(universe, iterations);
+
+let visual = renderVisual(results.data);
+console.log(visual);
+console.log(results.state.flashCount);
+console.log(results.state.current);
+console.log(results.state.allFlashed);
